5ea6988a4a04b6fb887bfeba964881ab3afbd641
kouki nakatsu <k-nakatsu@digeon.co>
2023-05-14 01:18:40 +0900

    App url

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+export const appURL = {
+  error: "*",
+  top: "/",
+};
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<h1 className="h-10 w-10 bg-black">dfds</h1>} />,
+    <Route path={appURL.top} element={<h1 className="h-10 w-10 bg-black">dfds</h1>} />,
   ),
 );
 
