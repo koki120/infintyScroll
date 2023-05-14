eb83d6bda5ecd5d982fb08936a0f8ae1d78dcb55
kouki nakatsu <k-nakatsu@digeon.co>
2023-05-14 14:33:06 +0900

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "ph-in": { max: "39.9375rem" },
+        "ph-out": { min: "40rem" },
+      },
+    },
   },
   plugins: [],
 };
